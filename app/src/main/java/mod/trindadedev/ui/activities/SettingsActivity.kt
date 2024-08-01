f40fef1b4072e03d53369b55a758c09098120636
aquilesTrindade <sempedra075@gmail.com>
2024-08-01 14:11:45 -0300

    fix: R import not found

--- app/src/main/java/mod/trindadedev/ui/activities/SettingsActivity.kt
+++ app/src/main/java/mod/trindadedev/ui/activities/SettingsActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
+import com.sketchware.remod.R
 import com.sketchware.remod.databinding.ActivitySettingsBinding
 
 import mod.trindadedev.ui.fragments.settings.appearance.SettingsAppearanceFragment
